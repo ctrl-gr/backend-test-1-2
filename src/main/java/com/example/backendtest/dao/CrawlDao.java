338fa24741ad86a17d6f90e53785fec3d189c161
ctrl-gr <giulia@giuliaredolfi.it>
2023-04-14 17:27:34 +0200

    Initial commit

---
+++ src/main/java/com/example/backendtest/dao/CrawlDao.java
@@ -0,0 +1,2 @@
+package com.example.backendtest.dao;public interface CrawlDao {
+}
---
+++ src/main/java/com/example/backendtest/service/CrawlServiceImpl.java
@@ -0,0 +1,7 @@
+package com.example.backendtest.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CrawlService {
+}
